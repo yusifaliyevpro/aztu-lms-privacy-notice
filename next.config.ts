2e5fab34f6fed8ea54ae39fae41253e2311379a0
Yusif Aliyev <yusifaliyevpro@gmail.com>
2026-08-28 19:39:05 +0400

    chore: update next.config.ts to include experimental features

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
+  partialPrefetching: true,
+  experimental: {
+    useTypeScriptCli: true,
+    turbopackRustReactCompiler: true,
+    useOffline: true,
+  },
 };
 
 export default nextConfig;
